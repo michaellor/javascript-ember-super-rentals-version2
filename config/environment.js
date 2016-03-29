731dbd90d776d15c5b62ec5034f2615b01973e42
erik tolentino and michael lor <michael.vang.1985@gmail.com>
2016-03-29 09:49:02 -0700

    add update functionality

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'super-rentals',
     environment: environment,
     contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
-    firebase: 'https://yvonna-erik.firebaseio.com/',
+    firebase: 'https://brilliant-heat-798.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
